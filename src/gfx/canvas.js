1dd2c59292cdd4034dea3813ce876009e39e3491
Jonah Simpson <jonahs99@gmail.com>
2019-02-02 11:19:56 -0500

    Stash before split

--- src/gfx/canvas.js
+++ src/gfx/canvas.js
@@ -4,6 +4,8 @@ export default function init_context() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
+	canvas.style.background = '#222'
+
     const context = canvas.getContext('2d')
 
     return context
